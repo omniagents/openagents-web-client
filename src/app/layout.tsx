d7f8506fdeb1dc76825e3663e3b03a8b7a2802fa
Raphael Shu <zhongzhu@amazon.com>
2025-03-14 14:36:58 -0700

    init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { OpenAgentsProvider } from "@/contexts/OpenAgentsContext";
 
